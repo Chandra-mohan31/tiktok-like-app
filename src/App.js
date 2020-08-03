19e4ea09ad0da6396ccd8ec38440971256bbb289
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2020-08-03 12:52:13 +0530

    finally the complete app with 2 videos

--- src/App.js
+++ src/App.js
@@ -1,67 +1,38 @@
-import React from 'react';
-
+import React, {useState,useEffect} from 'react';
+import db from "./firebase";
 import './App.css';
 import Video from "./Video";
 
 function App() {
+
+  const [videos,setVideos]=useState([]);
+  useEffect(() => {
+      db.collection('videos').onSnapshot(snapshot =>(
+        setVideos(snapshot.docs.map((doc)=>doc.data()))
+        
+      ))
+    
+  }, [videos])
   return (
     <div className="app">
       <div className="app__videos">
-       <Video
-           url="/Videos/video2.mp4"
-           channel="chand"
-           description="hey devs are u ready for the build"
-           song="vanthai ayiaaa vanthai ayiaa . . . ."
-           likes={200}
-           shares={24}
-           messages={56}
+      {videos.map(
+        ({ url,channel,description,song,likes,shares,messages})=>(
+          <Video 
+           url={url}
+           channel={channel}
+           description={description}
+           song={song}
+           likes={likes}
            
+           shares={shares}
+           messages={messages}
+          />
+        )
 
-       />
-        <Video
-           url="/Videos/video2.mp4"
-           channel="chand"
-           description="hey devs are u ready for the build"
-           song="vanthai ayiaaa vanthai ayiaa . . . ."
-           likes={200}
-           shares={24}
-           messages={56}
-           
 
-       />
-        <Video
-           url="/Videos/video2.mp4"
-           channel="chand"
-           description="hey devs are u ready for the build"
-           song="vanthai ayiaaa vanthai ayiaa . . . ."
-           likes={200}
-           shares={24}
-           messages={56}
-           
-
-       />
-        <Video
-           url="/Videos/video1.mp4"
-           channel="chand"
-           description="hey devs are u ready for the build"
-           song="vanthai ayiaaa vanthai ayiaa . . . ."
-           likes={200}
-           shares={24}
-           messages={56}
-           
-
-       />
-        <Video
-           url="/Videos/video2.mp4"
-           channel="chand"
-           description="hey devs are u ready for the build"
-           song="vanthai ayiaaa vanthai ayiaa . . . ."
-           likes={200}
-           shares={24}
-           messages={56}
-           
-
-       />
+      )}
+       
        
       </div>
     </div>
